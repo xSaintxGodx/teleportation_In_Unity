cc1df9dec8d8b0259fc5c225087ccdb2e00152b5
Wesley <wesleyhokage123@outlook.com>
2020-12-23 18:02:00 -0300

    Teleportation Mechanics
    Inclusion of the teleportation mechanics code (not yet complete)

---
+++ Teleportation/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 mousePos;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Teleporte();
+    }
+    private void Teleporte()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.position = MousePosition();
+        }
+
+    }
+
+    Vector2 MousePosition()
+    {
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        return mousePos;
+    }
+}
